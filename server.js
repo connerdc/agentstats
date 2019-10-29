486001e32d94f03dadc137b109d9e9716c68abaf
Conner Christopherson-Contractor <con900048@selectquotesenior.com>
2019-10-29 17:41:23 -0500

    working passing variables through render

--- server.js
+++ server.js
@@ -28,10 +28,44 @@ app.get('/', function (req, res) {
   })
 
   //render index page
-  res.render('index');
+  res.render('index', {State: "", 'Gal/Enroll': "", "User Email": "", "User Key": "" });
 })
 
-app.post('/', function (req, res, next) {
+function initializeConn(req, res, next) {
+  console.log("Callback 1");
+  next();
+}
+
+function galAndACDStates(req, res, next) {
+  var request = new mssql.Request(pool);
+  console.log("Callback 2");
+
+  //sql queries to run
+  var usersql = "select usr_key, usr_email, usr_first_name, usr_last_name from users where usr_active_flag = 1 and usr_delete_flag = 0 and usr_key ='" + req.body.agent + "' order by usr_first_name asc";
+  var pastduecalendarsql = "select eventcalendar.act_account_id as AccountID, eventcalendar.evc_title as EventTitle,eventcalendar.evc_specific_date_time_from_now as TimeOfEvent, eventcalendar.evc_dismissed as IsDismissed,eventcalendar.evc_google_api_log as GoogleAPILog, users.usr_email as UserEmail from eventcalendar join users on eventcalendar.usr_user_id = users.usr_key where usr_user_id in (select usr_key from users where usr_key ='" + req.body.agent + "' and evc_specific_date_time_from_now < getdate() and usr_active_flag = 1 and usr_delete_flag = 0)and evc_dismissed = 0 order by users.usr_email desc, evc_specific_date_time_from_now desc";
+  var galandacdsql = "select agent_group_name, agent_group_acd_flag, usr_email, usr_key from gal_agentgroups AS ag join gal_agent2agentgroups AS a2g on a2g.agent_group_id = ag.agent_group_id join users on usr_key = a2g.agent_id where  agent_group_inactive = 0 and agent_group_delete_flag = 0 and usr_key ='" + req.body.agent + "' order by agent_group_name asc";
+  var galandenrollstatessql = "select sta_full_name AS 'State', stl_type AS 'Gal/Enroll', usr_email AS 'User Email', usr_key AS 'User Key' from state_licensure join users on usr_key = stl_usr_key join states on sta_key = stl_sta_key where usr_key ='" + req.body.agent + "' order by stl_type, sta_full_name asc";
+  //, usr_email AS 'User Email', usr_key AS 'User Key' FOR ABOVE QUERY
+  request.query(galandenrollstatessql, function(err, recordset) {
+    console.log("SQL Query");
+    userResponse = recordset;
+    console.log(recordset);
+    next();
+  }); 
+}
+
+function renderStatistics(req, res, next) {
+  console.log("Callback 3");
+  console.log("Render response object:\n" + userResponse.recordset[1] + "\n");
+  //app.set('json spaces', 2);
+  //res.send(userResponse.recordset);
+  res.render("index", userResponse.recordset[1]);
+  //, {userResponse: "recordset"}
+}
+
+app.post('/', initializeConn, galAndACDStates, renderStatistics);
+
+/*app.post('/', function (req, res, next) {
   console.log("first middle ware");
   //if no pool connection exists for pool variable, connect pool for POST
   if(!pool) {
@@ -40,7 +74,7 @@ app.post('/', function (req, res, next) {
     })
   } 
   next();
- }, function (req, res, next) {
+}, function (req, res, next) {
   console.log("second middle ware");
   var request = new mssql.Request(pool);
 
@@ -48,30 +82,21 @@ app.post('/', function (req, res, next) {
   var usersql = "select usr_key, usr_email, usr_first_name, usr_last_name from users where usr_active_flag = 1 and usr_delete_flag = 0 and usr_email like '%" + req.body.agent + "%' order by usr_first_name asc";
   var galandacdsql = "select agent_group_name, agent_group_acd_flag, usr_email, usr_key from gal_agentgroups AS ag join gal_agent2agentgroups AS a2g on a2g.agent_group_id = ag.agent_group_id join users on usr_key = a2g.agent_id where  agent_group_inactive = 0 and agent_group_delete_flag = 0 and usr_email like '%" + req.body.agent + "%' order by agent_group_name asc";
   //var simplesql = "select top 1 * from users";
-  //console.log(galandacdsql);
   
-  //request for sql queries, populated from database based on above queries
-  /*
-    Problem right now is with below. Returned data from query is not passing to response in .render below. Solution could be using next() to call another middleware function to call query function that is seperated from this POST. I think it could be inside as well but will need to look at how variables are passing around inside this.
-  */
+  //Problem right now is with below. Returned data from query is not passing to response in .render below. Solution could be using next() to call another middleware function to call query function that is seperated from this POST. I think it could be inside as well but will need to look at how variables are passing around inside this.
+  next();
+}, function (req, res, next) {
   request.query(galandacdsql, function(err, recordset) {
       console.log("SQL Query");
       userResponse = recordset;
-      /* console.log("\nResponse Body:");
-      console.log(userResponse);
-      console.log("\nParsed Response:")
-      //below populates based off of index of record set returned from query. Most queries only return one row, so the index of this row is 0. 
-      for(var i=0; i<userResponse.recordset.length; i++) {
-        console.log("User Email: " + userResponse.recordset[i]["usr_email"] + "\nAgent Group Name: " + userResponse.recordset[i]["agent_group_name"] + "\nIs ACD Group: " + userResponse.recordset[i]["agent_group_acd_flag"]);
-      } */
       console.log(recordset);
   }); 
   next();
- }, function (req, res) {
+}, function (req, res) {
   console.log("third middle ware");
   console.log(userResponse);
-  res.render('index', userResponse.recordset[0]);  
-})
+  res.render('index', );  
+}) */
 
 app.listen(3000, function () {
   console.log('Example app listening on port 3000!')
